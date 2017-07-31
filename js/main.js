6ce1078cf71ff33f3ebe80b37ffeace9bb5dd8cc
szaroket <mpiestrzynska@interia.pl>
2017-07-31 16:12:03 +0200

    Add space ship to the game

--- js/main.js
+++ js/main.js
@@ -6,16 +6,24 @@ var game = new Phaser.Game(500, 400, Phaser.AUTO, null, {
     update: update
 });
 
+var ship;
+
 function preload() {
     //scale object
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = 'black';
+
+    //load image of the ship
+    game.load.image('ship', 'img/ship2.png');
+
 }
 
 function create() {
-
+    //create the ship
+    ship = game.add.sprite(game.world.width * 0.5, game.world.height - 40, 'ship');
+    ship.anchor.set(0.5);
 }
 
 function update() {
