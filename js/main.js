9798a07ff3caf73890b3fd5d353eabbbd33c971a
szaroket <mpiestrzynska@interia.pl>
2017-08-01 09:11:38 +0200

    Control the ship with the keyboard

--- js/main.js
+++ js/main.js
@@ -21,11 +21,30 @@ function preload() {
 }
 
 function create() {
+    //initialize the Arcade Physics engine
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     //create the ship
     ship = game.add.sprite(game.world.width * 0.5, game.world.height - 40, 'ship');
     ship.anchor.set(0.5);
+    game.physics.enable(ship, Phaser.Physics.ARCADE);
 }
 
 function update() {
-
+    cursors = game.input.keyboard.addKeys({
+        'shot': Phaser.Keyboard.SPACEBAR,
+        'left': Phaser.Keyboard.LEFT,
+        'right': Phaser.Keyboard.RIGHT
+    });
+    if (cursors.left.isDown) {
+        ship.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown) {
+        ship.body.velocity.x = 150;
+    }
+    else if (cursors.shot.isDown) {
+        
+    }
+    else {
+        ship.body.velocity.x = 0;
+    }
 }
